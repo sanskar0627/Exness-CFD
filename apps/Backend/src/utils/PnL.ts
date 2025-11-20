d0369cee5d8f77ab626b7dbe289baec6db0f4206
SanskarShukla <sanskar0627@gmail.com>
2025-11-20 19:21:16 +0530

    feat:add calculationof Profit and loss

---
+++ apps/Backend/src/utils/PnL.ts
@@ -0,0 +1,28 @@
+import { OrderType } from "../types";
+
+export function calculatePnLCents(
+  openPrice: number,
+  closePrice: number,
+  marginCent: number,
+  leverage: number,
+  side: OrderType
+): number {
+  let profitandLoss: bigint = BigInt(0);
+  const op = BigInt(openPrice); // current oprn price
+  const cp = BigInt(closePrice); // current clode price
+  const mc = BigInt(marginCent);
+  const lg = BigInt(leverage);
+  const PositionValue = mc * lg; // postion value is basiaclly purcahsing powwer  (margin × leverage)
+  //Caluclating price diffrence
+  const buyPrice: bigint = cp - op; //// Profit/Loss for LONG (buy) positions
+  const sellPrice: bigint = op - cp; // // Profit/Loss for Short  (sell) positions
+
+  if (side === "buy") {
+    //means we are calulating for long
+    profitandLoss = (PositionValue * buyPrice) / op;
+  } else if (side === "sell") {//we are calculating for the short
+    profitandLoss = (PositionValue * sellPrice) / op;
+  }
+
+  return Number(profitandLoss);
+}
