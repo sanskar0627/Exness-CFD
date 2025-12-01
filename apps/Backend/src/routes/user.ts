27581981b1154a11d8eafb6a3f5d89729ee942b1
SanskarShukla <sanskar0627@gmail.com>
2025-12-01 15:29:47 +0530

    feat(backend): complete Stage 10 Phase 1 & 2 - database schema and user

--- apps/Backend/src/routes/user.ts
+++ apps/Backend/src/routes/user.ts
@@ -8,51 +8,64 @@ import { authMiddleware } from "../middleware/auth";
 import type { SignupRequest, SigninRequest } from "../types";
 import { signupSchema, signinSchema } from "../types";
 import { fromInternalUSD } from "shared";
+import { prisma, Prisma } from "database";
 
 const UUID_NAMESPACE = "6ba7b810-9dad-11d1-80b4-00c04fd430c8";
 export const userRouter = Router(); // To organise ROutes
 
-userRouter.post("/signup", (req: Request, res: Response): void => {
-  try {
-    const { email, password } = req.body as SignupRequest;
-    console.log(`[SIGNUP] Attempt for email: ${email}`);
+userRouter.post(
+  "/signup",
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { email, password } = req.body as SignupRequest;
+      console.log(`[SIGNUP] Attempt for email: ${email}`);
 
-    //validate using zod
-    const validate = signupSchema.safeParse({ email, password });
-    if (!validate.success) {
-      res
-        .status(400)
-        .json({ error: "Invalid Input", details: validate.error.issues });
-      return;
-    }
-    const exsistingUser = findUser(email);
-    if (exsistingUser) {
-      console.log(`[SIGNUP] User already exists: ${email}`);
-      res.status(409).json({ error: "User already exists" });
-      return;
-    }
-    //if its a valid gmail then genarting a vlaid uuid
-    const UserId = uuidv5(email, UUID_NAMESPACE);
-    console.log(`[SIGNUP] Generated userId: ${UserId} for ${email}`);
+      //validate using zod
+      const validate = signupSchema.safeParse({ email, password });
+      if (!validate.success) {
+        res
+          .status(400)
+          .json({ error: "Invalid Input", details: validate.error.issues });
+        return;
+      }
+      const exsistingUser = await prisma.user.findUnique({ where: { email } });
+      if (exsistingUser) {
+        console.log(`[SIGNUP] User already exists: ${email}`);
+        res.status(409).json({ error: "User already exists" });
+        return;
+      }
+      //if its a valid gmail then genarting a vlaid uuid
+      const UserId = uuidv5(email, UUID_NAMESPACE);
+      console.log(`[SIGNUP] Generated userId: ${UserId} for ${email}`);
 
-    // Hash password before storing
-    const hashedPassword = hashPassword(password);
-    const newUser = CreateUser(UserId, email, hashedPassword);
-    const newToken = generateToken(UserId);
-    res.status(201).json({
-      message: "User created successfully",
-      userId: newUser.userId,
-      token: newToken,
-    });
-    console.log(`[SIGNUP] User created successfully: ${email}`);
-  } catch (err) {
-    console.error("[SIGNUP] Error", err);
-    res.status(500).json({ error: "INternal server Error in Signup" });
+      // Hash password before storing
+      const hashedPassword = hashPassword(password);
+      await prisma.user.create({
+        data: {
+          userId: UserId,
+          email: email,
+          password: hashedPassword,
+          balanceCents: 500000,
+        },
+      });
+      const newUser = CreateUser(UserId, email, hashedPassword);
+      const newToken = generateToken(UserId);
+
+      res.status(201).json({
+        message: "User created successfully",
+        userId: newUser.userId,
+        token: newToken,
+      });
+      console.log(`[SIGNUP] User created successfully: ${email}`);
+    } catch (err) {
+      console.error("[SIGNUP] Error", err);
+      res.status(500).json({ error: "INternal server Error in Signup" });
+    }
   }
-});
+);
 
 //SIGN IN ROutes
-userRouter.post("/signin", (req: Request, res: Response): void => {
+userRouter.post("/signin",async (req: Request, res: Response): Promise<void> => {
   try {
     const { email, password } = req.body as SigninRequest;
     const validate = signinSchema.safeParse({ email, password });
@@ -64,24 +77,31 @@ userRouter.post("/signin", (req: Request, res: Response): void => {
       return;
     }
     console.log(`[SIGNIN] Attempt for email: ${email}`);
-    const existingUser = findUser(email);
+    const user = await prisma.user.findUnique({
+    where: { email }
+  });
     //check that the email is valid or not
-    if (!existingUser) {
+    if (!user) {
       console.log(`[SIGNIN] User not found: ${email}`);
       res.status(401).json({ error: "Invalid credentials" });
       return;
     }
     //check the password is same or not using bcrypt
-    const isPasswordValid = comparePassword(password, existingUser.password);
+    const isPasswordValid = comparePassword(password, user.password);
     if (!isPasswordValid) {
       console.log(`[SIGNIN] Invalid password for: ${email}`);
       res.status(401).json({ error: "Invalid credentials" });
       return;
     }
-    const token = generateToken(existingUser.userId);
+    const token = generateToken(user.userId);
+    const inMemory = findUSerId(user.userId);
+    if (!inMemory) {
+    CreateUser(user.userId, user.email, user.password);
+    console.log(`[SIGNIN] User ${user.userId} loaded into memory from database`);
+  }
     res.status(200).json({
       message: "Login successful",
-      userId: existingUser.userId,
+      userId: user.userId,
       token: token,
     });
     console.log(`[SIGNIN] Login successful: ${email}`);
@@ -124,5 +144,5 @@ userRouter.get(
       console.error("[BALANCE] Error", err);
       res.status(500).json({ error: "internal serveral error" });
     }
-  },
+  }
 );
